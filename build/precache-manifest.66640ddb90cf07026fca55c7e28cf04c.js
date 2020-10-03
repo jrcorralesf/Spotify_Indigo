fa838c86038ec7f39818c58fff41d6da0fc92cc1
Julian Corrales <jrcorralesf@unal.edu.co>
2020-10-03 11:27:00 -0500

    Subida de la carpeta Build hecha con npm run build

---
+++ build/precache-manifest.66640ddb90cf07026fca55c7e28cf04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb5237dbbe963da8b4c749e208a9454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5aac9044a4578810ac",
+    "url": "/static/css/main.ba87f85f.chunk.css"
+  },
+  {
+    "revision": "a293e03713f98a5be6b9",
+    "url": "/static/js/2.f8d5d6a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f8d5d6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5aac9044a4578810ac",
+    "url": "/static/js/main.31386f5a.chunk.js"
+  },
+  {
+    "revision": "c19ec67689c185f11086",
+    "url": "/static/js/runtime-main.9a44ea14.js"
+  }
+]);
